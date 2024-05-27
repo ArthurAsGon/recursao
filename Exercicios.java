18bde6ebec73952825dd811ac51123bef326f2db
ArthurAsGon <webbmor2@gmail.com>
2024-05-27 18:04:16 -0300

    soma

--- Exercicios.java
+++ Exercicios.java
@@ -1,13 +1,14 @@
 public class Exercicios {
 
-    public boolean aux(int a, int d){
+    public static boolean aux(int a, int d){
 
         if(a == 0) return true;
         if(d == 0) return false;
         return aux(++a, --d);
 
     }
-    public boolean ex1(int x) throws Exception{
+    //é negativo
+    public static boolean ex1(int x){
 
         // se positivo retornar falso
         // se negativo retorna true
@@ -17,6 +18,23 @@ public class Exercicios {
 
 
     }
+    //somar
+    public static int ex2(int x1, int x2){
+
+        if (x2 == 0) return x1 ;
+        if(ex1(x2))
+        {
+            return ex2(--x1, ++x2);
+        }
+
+        return ex2(++x1, --x2);
+
+
+    }
+
+
+
+
 
 
 
--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Exercicios ex = new Exercicios();
-
         try {
             System.out.println(ex.ex1(7));
+            System.out.println(ex.ex2(5,5));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
